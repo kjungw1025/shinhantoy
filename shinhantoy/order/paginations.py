15f8de3f6e69143613d60c77849732fcb43fb697
kjungw1025 <kjungw1025@naver.com>
2023-01-25 23:57:51 +0900

    feat: paginations.py

--- shinhantoy/order/paginations.py
+++ shinhantoy/order/paginations.py
@@ -2,4 +2,4 @@ from rest_framework import pagination
 
 class OrderLargePagination(pagination.PageNumberPagination):
     # pass라고 두면 pagination 기본값으로 설정됨
-    page_size = 100000
+    page_size = 10
