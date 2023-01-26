92ef61f04d08fc899b78b99fb494bd08d92a58bd
kjungw1025 <kjungw1025@naver.com>
2023-01-26 23:04:04 +0900

    feat: shinhantoy

--- shinhantoy/order/admin.py
+++ shinhantoy/order/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order, Comment
 
 # Register your models here.
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
     pass
